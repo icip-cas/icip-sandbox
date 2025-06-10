c748cb3d34b83eac909c144049af8e1734aa01e1
zheng xin <510546841@qq.com>
2025-06-10 10:34:44 +0800

    update license

--- sandbox/server/sandbox_api.py
+++ sandbox/server/sandbox_api.py
@@ -1,3 +1,4 @@
+# Copyright 2025 Chinese Information Processing Laboratory, Institute of Software, Chinese Academy of Sciences.
 # Copyright 2024 Bytedance Ltd. and/or its affiliates
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
